919865e9640a67c8343ce6f7099acbdcf83b6acf
Stevanie Kanter <stevaniek@yahoo.com>
2021-07-09 20:21:01 -0400

    added the code for password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -10,5 +10,32 @@ function writePassword() {
 
 }
 
+// Adding the array for the password criteria
+// number charaters for the password 
+const numericChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// lowecase charaters
+const lowerCasedChar = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+  'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+];
+
+// uppercase charaters
+const upperCasedChar = [
+  'A', 'B', 'C', 'D', 'E',  'F',  'G',  'H',  'I',  'J',  'K',  'L',  'M',
+  'N',  'O',  'P',  'Q',  'R',  'S',  'T',  'U',  'V',  'W',  'X',  'Y',  'Z'
+];
+
+// special characters
+const specialChar= [
+  '@',  '%',  '+',  '\\',  '/',  "'",  '!',  '#',  '$',  '^',  '?',  ':',
+  ',',  ')',  '(',  '}',  '{',  ']',  '[',  '~',  '-',  '_',  '.'
+];
+
+// adding function code
+function generatePassword () {
+
+  let randomArray = [];
+  let password = '';
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
